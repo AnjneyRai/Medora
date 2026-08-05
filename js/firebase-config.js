f2d8c738ca2089d60aa2558e5685902b888ba33a
Anjney <anjneyrai81@gmail.com>
2026-08-05 23:37:53 +0530

    Added firebase

---
+++ js/firebase-config.js
@@ -0,0 +1,9 @@
+// Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDjOR_xZuepfkaHDZFxw-r498bRvrHRdRY",
+    authDomain: "medora-cyberknights.firebaseapp.com",
+    projectId: "medora-cyberknights",
+    storageBucket: "medora-cyberknights.firebasestorage.app",
+    messagingSenderId: "536762487303",
+    appId: "1:536762487303:web:26e43a0e67b83a366c50a2"
+  };
